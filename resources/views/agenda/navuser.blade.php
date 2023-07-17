bf372b6f4d6b777993986b9ef45e136899fcbda1
JuaNFRaN2031 <juanfran2002juanfran2002@hotmail.com>
2023-07-17 17:59:21 +0200

    .

--- resources/views/agenda/navuser.blade.php
+++ resources/views/agenda/navuser.blade.php
@@ -1,13 +1,16 @@
 <nav class="navbar navbar-light">
-    <a class="navbar-brand"><img id="icono" class="img-responsive"
+    <a href="{{ route('agenda.index') }}" class="navbar-brand"><img id="icono" class="img-responsive"
             src="https://cdn.icon-icons.com/icons2/403/PNG/512/phonebook_40497.png" style="width: 70px;"></a>
 
     <ul class="nav flex-column text-center">
         <li class="nav-item">
-            <span class="nav-link active">Bienvenido Jhonatan</span>
+            <span class="nav-link active">Bienvenido {{ Auth::user()->name }}</span>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Cerrar sesión</a>
+            <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Cerrar sesión</a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
         </li>
     </ul>
 
