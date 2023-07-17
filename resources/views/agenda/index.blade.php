d8809afe00b3ffbce6133ae128af635f042a7255
JuaNFRaN2031 <juanfran2002juanfran2002@hotmail.com>
2023-07-17 17:39:26 +0200

    .

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -39,21 +39,8 @@
         @endif
 
         <br>
-        <nav class="navbar navbar-light">
-            <a class="navbar-brand"><img id="icono" class="img-responsive"
-                    src="https://cdn.icon-icons.com/icons2/403/PNG/512/phonebook_40497.png" style="width: 70px;"></a>
 
-            <ul class="nav flex-column text-center">
-                <li class="nav-item">
-                    <span class="nav-link active">Bienvenido Jhonatan</span>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Cerrar sesión</a>
-                </li>
-            </ul>
-
-        </nav>
-
+        @include('agenda.navuser')
 
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
