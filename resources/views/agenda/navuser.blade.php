d99a4594757fe17cda072a647d14df39dbda77fb
JuaNFRaN2031 <juanfran2002juanfran2002@hotmail.com>
2023-07-18 13:11:39 +0200

    .

--- resources/views/agenda/navuser.blade.php
+++ resources/views/agenda/navuser.blade.php
@@ -8,9 +8,13 @@
         </li>
         <li class="nav-item">
             <a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Cerrar sesión</a>
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                Cerrar sesión
+            </a>
 
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
         </li>
     </ul>
 
